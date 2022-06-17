f7e9287b8a367b07fc9a632032620b499ca2fd1d
chiraleo2000 <87626767+chiraleo2000@users.noreply.github.com>
2022-06-17 08:58:11 +0700

    Update Joy_stick_PICO.py

--- Joy_stick_PICO.py
+++ Joy_stick_PICO.py
@@ -1,8 +1,11 @@
+#This is for testing ADC (Analog to Digital Convertor) in PICO.
+# Using ADC library from machine to setting pin to accept ADC.
+# Only specific pins can configurate to ADC pin and for Input only.
 from machine import Pin, ADC
 import utime
-xAxis = ADC(Pin(27))
-yAxis = ADC(Pin(26))
-button = Pin(16,Pin.IN, Pin.PULL_UP)
+xAxis = ADC(Pin(27)) # ADC pins compatible only
+yAxis = ADC(Pin(26)) # ADC pins conpatible only
+button = Pin(16,Pin.IN, Pin.PULL_UP) # Normal Pins
 while True:
     xValue = xAxis.read_u16()
     yValue = yAxis.read_u16()
